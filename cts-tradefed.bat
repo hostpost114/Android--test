2a4e691e61d8ca18c166c7610bff6d1fc84e069d
hostpost114 <31546102+hostpost114@users.noreply.github.com>
2018-01-19 10:00:47 +0800

    Add files via upload

---
+++ cts-tradefed.bat
@@ -0,0 +1,37 @@
+@echo off
+
+setlocal enabledelayedexpansion
+setlocal ENABLEEXTENSIONS
+
+set CTS_ROOT=%~dp0\..\..
+
+
+set JAR_DIR=%CTS_ROOT%\android-cts\tools
+
+
+set JARS=compatibility-common-util-tests.jar compatibility-host-util.jar compatibility-host-util-tests.jar compatibility-tradefed-tests.jar cts-tradefed.jar cts-tradefed-tests.jar host-libprotobuf-java-full.jar hosttestlib.jar tradefed.jar
+
+
+set DestJar=*.jar
+
+
+
+set JAR_PATH=.
+
+for %%i in (%JARS%) do (
+
+	set JAR_PATH=!JAR_PATH!;%%i
+
+)
+
+for  %%a in ("%CTS_ROOT%\android-cts\testcases\*.jar") do (
+
+    set JAR_PATH=!JAR_PATH!;%%a
+
+)
+
+
+java -Xmx1024m %RDBG_FLAG% -cp %JAR_PATH% -DCTS_ROOT=%CTS_ROOT% com.android.compatibility.common.tradefed.command.CompatibilityConsole %*
+
+pause
+

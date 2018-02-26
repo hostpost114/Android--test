4cda2622e32ddff52f409eb0495b439c90e3f23f
hostpost114 <31546102+hostpost114@users.noreply.github.com>
2018-02-26 20:40:48 +0800

    Update push.py

--- push.py
+++ push.py
@@ -1,3 +1,5 @@
+from multiprocessing import Pool
+
 import os ,time
 x=('adb devices')
 m=os.popen(x)
@@ -7,12 +9,28 @@ for lie in m.readlines():
     if "devices"not in lie:
         l=lie.split('device')[0]
         pp.append(l)
-# print(pp)
-
-
-for i in pp:
-    print(i)
+print(len(pp)-1)
+def push_media(device):
+    # for i in pp:
+    #     # print(i)
     pwd = os.getcwd()
-    print(pwd)
-    os.system('adb -s %s push %s/bbbs/4564 /sdcard/test/4564'%(i,pwd))
-    os.system('adb -s %s push %s/ppppp/999+ /sdcard/test/999+' %(i,pwd))
+    # print(pwd)
+    os.system('adb -s %s push %s/bbbs/4564 /sdcard/test/4564' % (device, pwd))
+    os.system('adb -s %s push %s/jdk-8u151-linux-x64.tar.gz /sdcard/test/jdk-8u151-linux-x64.tar.gz' % (device, pwd))
+    os.system('adb -s %s push %s/node-v9.4.0-linux-x64.tar.xz /sdcard/test/node-v9.4.0-linux-x64.tar.xz' % (device, pwd))
+    os.system('adb -s %s push %s/Postman-linux-x64-5.5.0.tar.gz /sdcard/test/Postman-linux-x64-5.5.0.tar.gz' % (device, pwd))
+    os.system('adb -s %s push %s/pycharm-professional-2017.3.2.tar.gz /sdcard/test/pycharm-professional-2017.3.2.tar.gz' % (device, pwd))
+    print(666666666)
+
+
+
+if __name__ == "__main__":
+    print("Current process is %d" % (os.getpid()))
+    p = Pool()
+    # for i in range(len(pp)-1):
+    for m in pp[0:len(pp)-1]:
+        p.apply_async(push_media, (m,))  # 增加新的进程
+            # print('this is m %s'%m)
+    p.close()  # 禁止在增加新的进程
+    p.join()
+    print("pool process done")
